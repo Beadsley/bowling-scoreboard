e701dfdc972d91ca1bf8677bc2e73f7335208483
beadsley <danbeadleson@gmail.com>
2020-03-19 07:27:00 +0100

    score board template added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import EnterScore from './components/EnterScore';
 import EnterName from './components/EnterName';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Scoreboard</h1>
       </header>
+      <EnterScore />
       <EnterName />
-     <EnterScore />
+
     </div>
   );
 }
--- client/src/components/EnterName.js
+++ client/src/components/EnterName.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/App.css';
 
 function EnterName() {
 
-    const [player, setPlayer] = useState([]);
-    console.log(player);
+    const [players, setPlayers] = useState([]);
+    const [boards, setBoards] = useState([]);
+
+    useEffect(() => {
+        generateBoard();
+    }, []);
 
     async function fetchData(name) {
+
         const response = await fetch('/api/player/', {
             method: 'post',
             body: JSON.stringify({
@@ -13,24 +19,42 @@ function EnterName() {
             })
         })
         const { id } = await response.json();
-        console.log(id);
-        setPlayer([...player, { id, name }])
+        setPlayers([...players, { id, name }])
+    }
+
+    function generateBoard() {
+        let array = [];
+        for (let index = 0; index <= 10; index++) {
+            array.push(
+                <div className="frame">
+                    <div className="frame-element">{index}</div>
+                    {index === 10 ? <div class="roll frame-element"> <div> 6 </div><div> 7 </div> <div> 8 </div>  </div> : <div class="roll frame-element"><div> 6 </div><div> 7 </div></div>}
+                    <div className="frame-element">{'score'}</div>
+                </div>
+            )
+        }
+        array.unshift(<div className="frame frame-element"> NAME</div>)
+        array.push(<div className="frame frame-element"> TOTAL</div>)
+        setBoards(array);
     }
 
     return (
         <>
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            fetchData(e.target[0].value)
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                fetchData(e.target[0].value)
 
-        }}>
-            <input type="text" name="name" id="nameInput" required placeholder="Add a to-do..."
-                autoComplete="off" />
-        </form>
+            }}>
+                <input type="text" name="name" id="nameInput" required placeholder="Add a player..."
+                    autoComplete="off" />
+            </form>
+            {players.map((x, i) => <div key={i} className="player-container"> {x.name}</div>)}
+            <div className="board-container">
+                {boards}
+            </div>
+
         </>
     )
 }
-
-
 
 export default EnterName;
