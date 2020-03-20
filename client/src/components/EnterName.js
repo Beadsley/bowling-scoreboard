710a0048b456b7a7bdbf423e416c0cfced64f302
beadsley <danbeadleson@gmail.com>
2020-03-20 08:40:32 +0100

    scored diplasyed on the scorboard after each frame

--- client/src/components/EnterName.js
+++ client/src/components/EnterName.js
@@ -11,13 +11,7 @@ function EnterName(game) {
         if (player) {
             generateBoard();
         }
-    }, [player]);
-
-    useEffect(() => {
-        if (game.frame > 1) {
-            fetchScores()
-        }
-    }, [game.frame]);
+    }, [player, game.frame]);
 
     async function addPlayer(name) {
 
@@ -37,28 +31,56 @@ function EnterName(game) {
     }
 
     async function fetchScores() {
-        console.log(game);
 
         const response = await fetch(`/api/player/scores/${game.currentPlayer}`)
         const result = await response.json();
-        console.log(result);
+        return result;
 
     }
 
-    function generateBoard() {
+    async function generateBoard() {
         let array = [];
-        for (let index = 1; index <= 10; index++) {
-            array.push(
-                <div className="frame">
-                    <div className="frame-element">{index}</div>
-                    {index === 10 ? <div className="roll frame-element"> <div>  </div><div>  </div> <div>  </div>  </div> : <div className="roll frame-element"><div>  </div><div>  </div></div>}
-                    <div className="frame-element">{'score'}</div>
-                </div>
-            )
+        if (game.frame > 1) {
+            console.log('heres');
+            const scores = await fetchScores();
+            console.log(scores);
+
+            for (let index = 0; index <= 9; index++) {
+                let roll1 = "";
+                let roll2 = "";
+                if (index < scores.length) {
+                    roll1 = scores[index][0]
+                    roll2 = scores[index][1]
+                    console.log(roll1, roll2);
+                }
+                array.push(
+                    <div className="frame">
+                        <div className="frame-element">{index + 1}</div>
+                        {index === 10 ? <div className="roll frame-element"> <div>  </div><div>  </div> <div>  </div>  </div> : <div className="roll frame-element"><div>{roll1}</div><div> {roll2} </div></div>}
+                        <div className="frame-element">{'score'}</div>
+                    </div>
+                )
+            }
+            array.unshift(<div className="frame frame-element name"> {player.name}</div>)
+            array.push(<div className="frame frame-element"> TOTAL</div>)
+            setBoards([array]);
+
         }
-        array.unshift(<div className="frame frame-element name"> {player.name}</div>)
-        array.push(<div className="frame frame-element"> TOTAL</div>)
-        setBoards([...boards, array]);
+        else {
+            for (let index = 1; index <= 10; index++) {
+                array.push(
+                    <div className="frame">
+                        <div className="frame-element">{index}</div>
+                        {index === 10 ? <div className="roll frame-element"> <div>  </div><div>  </div> <div>  </div>  </div> : <div className="roll frame-element"><div>  </div><div>  </div></div>}
+                        <div className="frame-element">{'score'}</div>
+                    </div>
+                )
+            }
+            array.unshift(<div className="frame frame-element name"> {player.name}</div>)
+            array.push(<div className="frame frame-element"> TOTAL</div>)
+            setBoards([...boards, array]);
+        }
+
     }
 
     return (
