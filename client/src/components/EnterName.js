45754d3b04f149237eff04ce98d84cbb824f60fa
beadsley <danbeadleson@gmail.com>
2020-03-21 16:10:13 +0100

    score updated after every 2 rolls

--- client/src/components/EnterName.js
+++ client/src/components/EnterName.js
@@ -5,18 +5,17 @@ import '../styles/App.css';
 function EnterName(game) {
 
     const alert = useAlert()
-    console.log(game.players);  
-    
+
 
     const [boards, setBoards] = useState([]);
 
     useEffect(() => {
 
-        if (game.players.length!==0) {
+        if (game.players.length !== 0) {
 
             generateBoard();
         }
-    }, [game.players, game.currentPlayer, game.frame]);
+    }, [game.players, game.currentPlayer]);
 
     async function createPlayer(name) {
 
@@ -33,85 +32,105 @@ function EnterName(game) {
         game.addPlayer(id, name)
     }
 
-    async function fetchScores() {
+    async function fetchScores(id) {
 
-        const response = await fetch(`/api/player/scores/${game.currentPlayer.id}`)
+        const response = await fetch(`/api/player/scores/${id}`)
         const result = await response.json();
         return result;
 
     }
 
-    async function fetchTotalScore() {
+    async function fetchTotalScore(id) {
 
-        const response = await fetch(`/api/player/total/${game.currentPlayer.id}`)
+        const response = await fetch(`/api/player/total/${id}`)
         const result = await response.json();
         return result;
 
     }
 
-    async function fetchConsecutiveScores() {
+    async function fetchConsecutiveScores(id) {
 
-        const response = await fetch(`/api/player/consecutiveScores/${game.currentPlayer.id}`)
+        const response = await fetch(`/api/player/consecutiveScores/${id}`)
         const result = await response.json();
         return result;
 
     }
 
-    async function generateBoard() {
-        let array = [];
-        if (game.frame > 1) {
-
-            const scores = await fetchScores();
-            const consecutiveScores = await fetchConsecutiveScores();
-            console.log(scores);
-
-            for (let index = 0; index <= 9; index++) {
-                let roll1 = "";
-                let roll2 = "";
-                let consecutiveScore = ".";
-                if (index < scores.length) {
-                    roll1 = scores[index][0]
-                    roll2 = scores[index][1]
-                    consecutiveScore = consecutiveScores[index]
-                }
-                
-                array.push(
-                    <div className="frame">
-                        <div className="frame-element">{index + 1}</div>
-                        {index === 10 ? <div className="roll frame-element"> <div>  </div><div>  </div> <div>  </div>  </div> : <div className="roll frame-element"><div>{roll1}</div><div> {roll2} </div></div>}
-                        <div className="frame-element">{consecutiveScore}</div>
-                    </div>
-                )
-            }
-            const total = await fetchTotalScore();
-            const {name} = game.players.find(player => player.id === game.currentPlayer.id);
-
-            array.unshift(<div className="frame frame-element name"> {name}</div>)
-            array.push(<div className="frame frame-element"> {total}</div>)
-            const index = boards.findIndex(board => board.id == game.currentPlayer.id);
-            boards.splice(index, 1, { id: game.currentPlayer.id, score: array });
-            setBoards([...boards]);
-
-        }
-        else if (game.frame === 1 && game.started) {
-            for (let index = 1; index <= 10; index++) {
-                array.push(
-                    <div className="frame">
-                        <div className="frame-element">{index}</div>
-                        {index === 10 ? <div className="roll frame-element"> <div>  </div><div>  </div> <div>  </div>  </div> : <div className="roll frame-element"><div>  </div><div>  </div></div>}
-                        <div className="frame-element">{'score'}</div>
-                    </div>
-                )
-            }
-            const {name} = game.players.find(player => player.id === game.currentPlayer.id);
+     function generateBoard() {
+        
+        if (game.started) {
+            console.log('called');
             
-            array.unshift(<div className="frame frame-element name"> {name}</div>)
-            array.push(<div className="frame frame-element"> TOTAL</div>)
-            const index = boards.findIndex(board => board.id == game.currentPlayer.id);
-            boards.splice(index, 1, { id: game.currentPlayer.id, score: array });
-            setBoards([...boards]);
+            game.players.forEach(async (player) => {
+                let array = [];
+                console.log(player);
+                
+                const scores = await fetchScores(player.id);
+                const consecutiveScores = await fetchConsecutiveScores(player.id);
+                
+                for (let index = 0; index <= 9; index++) {
+                    let roll1 = "";
+                    let roll2 = "";
+                    let consecutiveScore = ".";
+                    if (scores.error !== 'Game hasn\'t started yet!') {
+                        if (index < scores.length) {
+                            roll1 = scores[index][0]
+                            roll2 = scores[index][1]
+                            consecutiveScore = consecutiveScores[index]
+                        }
+                    }
+    
+                    array.push(
+                        <div className="frame">
+                            <div className="frame-element">{index + 1}</div>
+                            {index === 10 ? <div className="roll frame-element"> <div>  </div><div>  </div> <div>  </div>  </div> : <div className="roll frame-element"><div>{roll1}</div><div> {roll2} </div></div>}
+                            <div className="frame-element">{consecutiveScore}</div>
+                        </div>
+                    )
+                }
+                const total = await fetchTotalScore(player.id);
+               // const { name } = game.players.find(player => player.id === game.currentPlayer.id);
+    
+                array.unshift(<div className="frame frame-element name"> {player.name}</div>)
+                if (scores.error !== 'Game hasn\'t started yet!') {
+                    array.push(<div className="frame frame-element"> {total}</div>)
+                }
+                else {
+                    array.push(<div className="frame frame-element"> TOTAL</div>)
+                }
+                const index = boards.findIndex(board => board.id === player.id);
+                console.log(index);
+                console.log(array);
+                
+                boards.splice(index, 1, { id: player.id, score: array });
+
+                
+                setBoards([...boards]);
+            });
+
+
+
         }
+        // else if (game.frame === 1 && game.started) {
+        //     for (let index = 1; index <= 10; index++) {
+        //         array.push(
+        //             <div className="frame">
+        //                 <div className="frame-element">{index}</div>
+        //                 {index === 10 ? <div className="roll frame-element"> <div>  </div><div>  </div> <div>  </div>  </div> : <div className="roll frame-element"><div>  </div><div>  </div></div>}
+        //                 <div className="frame-element">{'score'}</div>
+        //             </div>
+        //         )
+        //     }
+        //     const { name } = game.players.find(player => player.id === game.currentPlayer.id);
+
+        //     array.unshift(<div className="frame frame-element name"> {name}</div>)
+        //     array.push(<div className="frame frame-element"> TOTAL</div>)
+        //     const index = boards.findIndex(board => board.id == game.currentPlayer.id);
+        //     boards.splice(index, 1, { id: game.currentPlayer.id, score: array });
+        //     setBoards([...boards]);
+        // }
         else {
+            let array = [];
             for (let index = 1; index <= 10; index++) {
                 array.push(
                     <div className="frame">
@@ -121,9 +140,9 @@ function EnterName(game) {
                     </div>
                 )
             }
-            array.unshift(<div className="frame frame-element name"> {game.players[game.players.length-1].name} </div>)
+            array.unshift(<div className="frame frame-element name"> {game.players[game.players.length - 1].name} </div>)
             array.push(<div className="frame frame-element"> TOTAL</div>)
-            setBoards([...boards, { id: game.players[game.players.length-1].id, score: array }]);
+            setBoards([...boards, { id: game.players[game.players.length - 1].id, score: array }]);
         }
 
     }
@@ -138,7 +157,7 @@ function EnterName(game) {
                 <input type="text" name="name" id="nameInput" required placeholder="Add a player..."
                     autoComplete="off" maxlength="10 " />
             </form>
-            <button onClick={game.players.length!==0 ? game.startGame : () => { alert.show('add a player...') }}>Start game</button>
+            <button onClick={game.players.length !== 0 ? game.startGame : () => { alert.show('add a player...') }}>Start game</button>
             <div className="boards-container">
                 {boards.map(board => <div className="board-container">{board.score}</div>)}
             </div>
